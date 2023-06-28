6216ab5d79d3e845e6de2be45a27bdd9207879ea
Junhui Ji <junhui.ji@aminer.cn>
2023-06-28 15:04:25 +0800

    test wds

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import webdataset as wds
 url = "/gpfs/gpfs1/zphz/img_datasets/laion115m/part-00001/100045.tar"
-dataset = wds.WebDataset(url).shuffle(1000).decode("rgb").to_tuple("png", "json")
+dataset = wds.WebDataset(url)
 
-for image, json in dataset:
+for sample in dataset:
     break
-print(json)
+print(sample)
