a1ba5a7d1775f51cf227d545e3fa682b493afbfb
Junhui Ji <junhui.ji@aminer.cn>
2023-06-28 14:37:21 +0800

    test wds

---
+++ test.py
@@ -0,0 +1,7 @@
+import webdataset as wds
+url = "/gpfs/gpfs1/zphz/img_datasets/laion115m/part-00001"
+dataset = wds.WebDataset(url).shuffle(1000).decode("rgb").to_tuple("png", "json")
+
+for image, json in dataset:
+    break
+print(json)
