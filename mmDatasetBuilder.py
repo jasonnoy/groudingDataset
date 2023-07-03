0871e1dfe0f46c04271331c0742ea115c13faf6e
Junhui Ji <junhui.ji@aminer.cn>
2023-07-03 18:00:57 +0800

    init

--- mmDatasetBuilder.py
+++ mmDatasetBuilder.py
@@ -1,3 +1,26 @@
 from GLIP import *
+import numpy as np
+from PIL import Image
 
 
+def output_decorator(bboxes, labels):
+    res = []
+    for idx, box in enumerate(bboxes):
+        top_left, bottom_right = box[:2].tolist(), box[2:].tolist()
+        res.append(top_left+bottom_right+[labels[idx]])
+    return res
+
+def load(path):
+    pil_image = Image.open(path).convert("RGB")
+    # convert to BGR format
+    image = np.array(pil_image)[:, :, [2, 1, 0]]
+    return image
+
+
+if __name__ == "__main__":
+    image = load('test.jpg')
+    caption = 'bobble heads on top of the shelf'
+    result, pred = glip_demo.run_on_web_image(image, caption, 0.5)
+    bboxes = pred.bbox
+    labels = pred.get_field("labels")
+    print(output_decorator(bboxes, labels))
--- test.py
+++ test.py
@@ -1,11 +1,19 @@
-import webdataset as wds
-url = "/gpfs/gpfs1/zphz/img_datasets/laion115m/part-00001/100045.tar"
-dataset = wds.WebDataset(url)
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
 
-for sample in dataset:
-    break
+pylab.rcParams['figure.figsize'] = 20, 12
 
-for k,v in sample.items():
-    if k == "jpg":
-        continue
-    print("k = {}, v = {}".format(k,v))
+
+def imshow(img, caption):
+    plt.imshow(img[:, :, [2, 1, 0]])
+    plt.axis("off")
+    plt.figtext(0.5, 0.09, caption, wrap=True, horizontalalignment='center', fontsize=20)
+
+
+if __name__ == "__main__":
+    caption = 'bobble heads on top of the shelf'
+    img = np.load("result.npy")
+    imshow(img, caption)
+    plt.show()
