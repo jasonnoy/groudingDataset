fb2e1935a721025cfa70b12a606478d7663fcdd8
Junhui Ji <junhui.ji@aminer.cn>
2023-07-05 12:02:02 +0800

    init glip2

--- mmDatasetBuilder.py
+++ mmDatasetBuilder.py
@@ -72,6 +72,8 @@ def parse_and_grounding_single_class(img, caption, idx, nlp, output_path):
     for noun_chunk in doc.noun_chunks:
         chunk_text = noun_chunk.text
         result, pred = glip_demo.run_on_image([image], [chunk_text], 0.55)
+        print("result:", result)
+        print("pred:", pred)
         # if no detection
         if len(pred.bbox) == 0:
             continue
