74cd98a14f4c341577eb3f4b3be84724a0f99d3e
Junhui Ji <junhui.ji@aminer.cn>
2023-07-05 06:01:32 +0000

    debug

--- GLIP/grounder.py
+++ GLIP/grounder.py
@@ -6,7 +6,7 @@ from GLIP.maskrcnn_benchmark.config import cfg
 from GLIP.maskrcnn_benchmark.engine.predictor_glip import GLIPDemo
 
 
-config_file = "./configs/pretrain/glip_Swin_L.yaml"
+config_file = "./GLIP/configs/pretrain/glip_Swin_L.yaml"
 weight_file = "/zphz/jjh/models/glip/MODEL/glip_large_model.pth"
 cfg.local_rank = 0
 cfg.num_gpus = 2
