d5799eb8ca1147ec2a30926e7a62193f5100f13b
Junhui Ji <junhui.ji@aminer.cn>
2023-07-06 18:20:01 +0800

    debug

--- test.py
+++ test.py
@@ -26,6 +26,8 @@ if __name__ == "__main__":
                     size = (int(meta_data['width']), int(meta_data['height']))
                     index = data['id']
                     sample_id = meta_data['SAMPLE_ID']
+                    print(index)
+                    print(sample_id)
                     assert (str(index) == str(sample_id))
                     print("id:", index)
                     print("sample id:", sample_id)
