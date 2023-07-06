691a63eadc2dc9eb2ad44804c90ad3faac5a15e9
Junhui Ji <junhui.ji@aminer.cn>
2023-07-06 17:58:55 +0800

    debug

--- test.py
+++ test.py
@@ -21,15 +21,16 @@ if __name__ == "__main__":
         with open(os.path.join(input_path, meta_filename), 'r', encoding='utf-8') as f1, open(os.path.join(output_path, meta_filename), 'a', encoding='utf-8') as f2:
             for data, line in zip(tar_dataset, f1):
                 meta_data = json.loads(line)
-                size = (meta_data['width'], meta_data['height'])
-                # print("data:", data)
-                image_b = data['jpg']
-                image = Image.frombytes(mode="RGB", data=image_b, size=size)
-                image.save("test.png", format="png")
-                caption = data['txt']
-                index = data['id']
-                # ret = parse_and_grounding_multi_class(image, caption, str(idx), nlp, output_path, True)
-                # meta_data.update(ret)
+                if meta_data['status'] is "success":
+                    size = (int(meta_data['width']), int(meta_data['height']))
+                    # print("data:", data)
+                    image_b = data['jpg']
+                    image = Image.frombytes(mode="RGB", data=image_b, size=size)
+                    image.save("test.png", format="png")
+                    caption = data['txt']
+                    index = data['id']
+                    # ret = parse_and_grounding_multi_class(image, caption, str(idx), nlp, output_path, True)
+                    # meta_data.update(ret)
                 f2.write(json.dumps(meta_data, ensure_ascii=False) + '\n')
                 break
         f1.close()
