8fee8c79171ff0aa6c8fc042856d69f21c005bc2
Junhui Ji <junhui.ji@aminer.cn>
2023-07-11 14:48:16 +0800

    debug

--- GLIP/maskrcnn_benchmark/structures/image_list.py
+++ GLIP/maskrcnn_benchmark/structures/image_list.py
@@ -61,7 +61,7 @@ def to_image_list(tensors, size_divisible=0, padding=False):
             for img, pad_img in zip(tensors, batched_imgs):
                 pad_img[: img.shape[0], : img.shape[1], : img.shape[2]].copy_(img)
         else:
-            batched_imgs = torch.concat(tensors)
+            batched_imgs = tensors
         image_sizes = [im.shape[-2:] for im in tensors]
         return ImageList(batched_imgs, image_sizes)
     else:
