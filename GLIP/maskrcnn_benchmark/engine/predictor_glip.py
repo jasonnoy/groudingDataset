45ef6640db0d2acb10eaf83dcc1e229fa1c9a832
Junhui Ji <junhui.ji@aminer.cn>
2023-07-11 14:28:29 +0800

    debug

--- GLIP/maskrcnn_benchmark/engine/predictor_glip.py
+++ GLIP/maskrcnn_benchmark/engine/predictor_glip.py
@@ -172,6 +172,7 @@ class GLIPDemo(object):
                               positive_map_label_to_tokens,
                               thresh=0.5,
                               save_img=False):
+        images.to(self.device)
         predictions = self.model(images, new_entities, positive_map_label_to_tokens)
         top_predictions = [self._post_process(prediction, thresh) for prediction in predictions]
         results = None
