ec2ba586a3085f4d44cc9eeefb812a5d67ddb41d
Junhui Ji <junhui.ji@aminer.cn>
2023-07-12 19:50:41 +0800

    debug

--- GLIP/maskrcnn_benchmark/modeling/detector/generalized_vl_rcnn.py
+++ GLIP/maskrcnn_benchmark/modeling/detector/generalized_vl_rcnn.py
@@ -252,13 +252,14 @@ class GeneralizedVLRCNN(nn.Module):
 
         # visual embedding
         swint_feature_c4 = None
-        print("language_dict_features:", language_dict_features)
+        # print("language_dict_features:", language_dict_features)
         if 'vl' in self.cfg.MODEL.SWINT.VERSION:
             # the backbone only updates the "hidden" field in language_dict_features
             inputs = {"img": images.tensors, "lang": language_dict_features}
             visual_features, language_dict_features, swint_feature_c4 = self.backbone(inputs)
         else:
             visual_features = self.backbone(images.tensors)
+        print("visual_features:", visual_features)
         # rpn force boxes
         if targets:
             targets = [target.to(device)
