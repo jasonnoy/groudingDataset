8aa85b1a12270f033fc9d23f572c74bd54e84d19
Junhui Ji <junhui.ji@aminer.cn>
2023-07-13 16:37:39 +0800

    debug

--- GLIP/maskrcnn_benchmark/data/datasets/laion.py
+++ GLIP/maskrcnn_benchmark/data/datasets/laion.py
@@ -120,6 +120,7 @@ class Laion(data.Dataset):
         image_shape = image.size
         image_resize_shape = compute_image_shape(image_shape)
         image = image.resize(image_resize_shape)
+        image = np.array(image)[:, :, [2, 1, 0]]
 
         if self.transform is not None:
             image = self.transform(image)
