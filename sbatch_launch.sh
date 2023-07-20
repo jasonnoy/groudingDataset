396c6cfdd902c444a41bd34be833f6ed89024683
Junhui Ji <junhui.ji@aminer.cn>
2023-07-20 19:30:36 +0800

    debug

--- sbatch_launch.sh
+++ sbatch_launch.sh
@@ -3,8 +3,9 @@
 #SBATCH --output=./logs/build_bounding_dataset_laion_aes_%j.out
 #SBATCH --error=./logs/build_bounding_dataset_laion_aes_%j.err
 #SBATCH --nodes=1
+#SBATCH --gres=gpu:8
 #SBATCH --ntasks-per-node=8
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=3
 #SBATCH --partition=dev
 #SBATCH --export=ALL
 #SBATCH --exclude=1hlo8k81jmmul-0,1m5qco0g209qm-0,2fpm7g8mhc7m7-0,c89rgca402ma5-0
