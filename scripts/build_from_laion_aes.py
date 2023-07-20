cd32fa5d56fb3facc1da1f3908cfb44aa75cf99d
Junhui Ji <junhui.ji@aminer.cn>
2023-07-20 15:54:26 +0800

    debug

--- scripts/build_from_laion_aes.py
+++ scripts/build_from_laion_aes.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     for filename in files:
         tar_path = os.path.join(input_path, filename)
         res = {}
-        batch_size = 5
+        batch_size = 7
         laion_dataset = Laion(filename, input_path, nlp, tokenizer, transforms=glip_demo.transforms)
         meta_filename = "{}.meta.jsonl".format(filename)
         print("processing {}".format(filename))
