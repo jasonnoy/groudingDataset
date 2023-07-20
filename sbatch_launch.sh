705c35b1aead25e5adc446dcf33a3a5b9d70da68
Junhui Ji <junhui.ji@aminer.cn>
2023-07-20 18:35:47 +0800

    debug

--- sbatch_launch.sh
+++ sbatch_launch.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=./logs/build_bounding_dataset_laion_aes_%j.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=3
 #SBATCH --partition=dev
 #SBATCH --export=ALL
 #SBATCH --exclude=1hlo8k81jmmul-0,1m5qco0g209qm-0,2fpm7g8mhc7m7-0,c89rgca402ma5-0
