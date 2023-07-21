7590265aa47290f8b1a5946a69bbe22e5ef9ca82
Junhui Ji <junhui.ji@aminer.cn>
2023-07-21 14:24:30 +0800

    add ningxia

--- sbatch_launch.sh
+++ sbatch_launch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=build_bounding_dataset_laion_aes
-#SBATCH --output=./logs/build_bounding_dataset_laion_aes_%j.out
-#SBATCH --error=./logs/build_bounding_dataset_laion_aes_%j.err
-#SBATCH --nodes=4
-#SBATCH --gres=gpu:8
-#SBATCH --ntasks-per-node=8
+#SBATCH --job-name=build_bounding_dataset_laion_115m
+#SBATCH --output=./logs/build_bounding_dataset_laion_115m_%j.out
+#SBATCH --error=./logs/build_bounding_dataset_laion_115m_%j.err
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=3
 #SBATCH --partition=dev
 #SBATCH --export=ALL
-#SBATCH --exclude=1hlo8k81jmmul-0,1m5qco0g209qm-0,2fpm7g8mhc7m7-0,c89rgca402ma5-0
 
-srun experiments/build_from_laion_aes_jinan.sh
+srun experiments/build_from_laion_aes_ningxia.sh
+#srun experiments/build_from_laion_aes_jinan.sh
 echo "Done with job $SLURM_JOB_ID"
