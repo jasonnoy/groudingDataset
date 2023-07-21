d4a511f2eafd4897c60465aadde0b2af26a10638
Junhui Ji <junhui.ji@aminer.cn>
2023-07-21 17:39:33 +0800

    debug

--- GLIP/maskrcnn_benchmark/utils/imports.py
+++ GLIP/maskrcnn_benchmark/utils/imports.py
@@ -18,6 +18,19 @@ import torch
 # else:
 #     import imp
 
-def import_file(module_name, file_path, make_importable=None):
-    module = imp.load_source(module_name, file_path)
+# def import_file(module_name, file_path, make_importable=None):
+#     module = imp.load_source(module_name, file_path)
+#     return module
+
+import importlib
+import importlib.util
+import sys
+
+
+def import_file(module_name, file_path, make_importable=False):
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    if make_importable:
+        sys.modules[module_name] = module
     return module
