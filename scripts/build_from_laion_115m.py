996179745b9fd8462521c2e937c6f05ea55be8be
Junhui Ji <junhui.ji@aminer.cn>
2023-07-21 14:20:57 +0800

    add ningxia

--- scripts/build_from_laion_115m.py
+++ scripts/build_from_laion_115m.py
@@ -1,9 +1,15 @@
 import argparse
 import torch
 import json
+import os
+import sys
 import spacy
-from transformers import AutoTokenizer
 from tqdm import tqdm
+import webdataset
+
+sys.path.append(os.getcwd())
+sys.path.append(os.path.join(os.getcwd(), "GLIP"))
+
 from GLIP import *
 from dataset_builder import *
 
@@ -20,13 +26,14 @@ if __name__ == "__main__":
     os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:128'
 
     config_file = "../GLIP/configs/pretrain/glip_Swin_L.yaml"
-    weight_file = "/gpfs/gpfs1/zphz/jjh/models/glip/MODEL/glip_large_model.pth"
+    weight_file = "/nxchinamobile2/shared/official_pretrains/hf_home/GLIP-L/glip_large_model.pth"
     cfg.local_rank = args.local_rank
     cfg.num_gpus = 1
     cfg.merge_from_file(config_file)
     cfg.merge_from_list(["MODEL.WEIGHT", weight_file])
     cfg.merge_from_list(["MODEL.DEVICE", "cuda:{}".format(args.local_rank)])
     torch.cuda.set_device(args.local_rank)
+    cfg.MODEL.LANGUAGE_BACKBONE.LOCAL_PATH = "/nxchinamobile2/shared/official_pretrains/hf_home/bert-base-uncased"
     print("model device:",  cfg.MODEL.DEVICE)
     print("cuda device:", torch.cuda.current_device())
     rank = args.rank
@@ -40,44 +47,53 @@ if __name__ == "__main__":
     )
 
     nlp = spacy.load("en_core_web_trf")
-    input_path = "/gpfs/gpfs1/zphz/img_datasets/laion115m/part-00032"
-    output_path = "/gpfs/gpfs1/zphz/jjh/test_dataset/part-00032"
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
-    total_ids = get_id_list(input_path)
-    part_size = len(total_ids) // max((world_size-1), 1)
-    start = rank*part_size
-    end = min((rank+1)*part_size, len(total_ids))
-    ids = total_ids[start:end]
-    for idx in ids:
-        idx = int(idx)
-        res = {}
-        tokenizer = AutoTokenizer.from_pretrained("/gpfs/gpfs1/zphz/official_pretrains/hugging_face/bert-base-uncased")
-        batch_size = 5
-        laion_dataset = Laion(str(idx), input_path, nlp, tokenizer, transforms=glip_demo.transforms)
-        meta_filename = "{}.meta.jsonl".format(idx)
-        print("processing {}".format(idx))
-        groundings = batch_parse_and_grounding_multi_class(laion_dataset, batch_size=batch_size, save_img=False, output_path=output_path)
-        output_meta_path = os.path.join(output_path, meta_filename)
-        if os.path.exists(output_meta_path):
-            os.remove(output_meta_path)
-        with open(os.path.join(input_path, meta_filename), 'r', encoding='utf-8') as f1, open(output_meta_path, 'a', encoding='utf-8') as f2:
-            grounding_iter = iter(groundings)
-            for i, line in tqdm(enumerate(f1)):
-                meta_data = json.loads(line)
-                if meta_data['status'] == "success":
-                    grounding = next(grounding_iter)
-                    image_id = grounding['SAMPLE_ID']
-                    sample_id = meta_data['SAMPLE_ID']
-                    if str(image_id) != str(sample_id):
-                        print("index:{}\n sample_id:{}".format(str(image_id), str(sample_id)))
-                    meta_data.update(grounding)
-                    meta_data['annot_caption'] = build_training_text(record=meta_data)
-                else:
-                    meta_data['grounding'] = None
-                    meta_data['annot_caption'] = None
-                    loc_pos_list = None
-                f2.write(json.dumps(meta_data, ensure_ascii=False) + '\n')
-        f1.close()
-        f2.close()
-    print("done")
+    input_path = "/nxchinamobile2/shared/img_datasets/laion115m"
+    output_path = "/nxchinamobile2/shared/jjh/laion115m"
+    map_name = "file_map_laion_synthetic_filtered_large.json"
+    with open(os.path.join(input_path, map_name), 'r', encoding='utf-8') as f:
+        data = f.read()
+        data = json.loads(data)
+    f.close()
+    dirs = data[map_name]
+    dir_size = len(dirs) // max((world_size//8-1), 1)
+    dir_start = rank*dir_size
+    dir_end = (rank+1)*dir_start
+    select_dirs = dirs[dir_start:dir_end]
+    for cur_dir in select_dirs:
+        output_dir_path = os.path.join(output_path, str(cur_dir))
+        input_dir_path = os.path.join(input_path, str(cur_dir))
+
+        if not os.path.exists(output_dir_path):
+            os.mkdir(output_dir_path)
+        tar_files = get_id_list(input_dir_path)
+        for tar_file in tar_files:
+            idx = int(tar_file[:-4])
+            res = {}
+            batch_size = 10
+            laion_dataset = webdataset.WebDataset(os.path.join(input_dir_path, tar_file))
+            meta_filename = "{}.meta.jsonl".format(idx)
+            print("processing {}".format(idx))
+            groundings = batch_parse_and_grounding_multi_class(glip_demo, laion_dataset, batch_size=batch_size, save_img=False, output_path=output_dir_path)
+            output_meta_path = os.path.join(output_dir_path, meta_filename)
+            if os.path.exists(output_meta_path):
+                os.remove(output_meta_path)
+            with open(os.path.join(input_path, meta_filename), 'r', encoding='utf-8') as f1, open(output_meta_path, 'a', encoding='utf-8') as f2:
+                grounding_iter = iter(groundings)
+                for i, line in tqdm(enumerate(f1)):
+                    meta_data = json.loads(line)
+                    if meta_data['status'] == "success":
+                        grounding = next(grounding_iter)
+                        image_id = grounding['SAMPLE_ID']
+                        sample_id = meta_data['SAMPLE_ID']
+                        if str(image_id) != str(sample_id):
+                            print("index:{}\n sample_id:{}".format(str(image_id), str(sample_id)))
+                        meta_data.update(grounding)
+                        meta_data['annot_caption'] = build_training_text(record=meta_data)
+                    else:
+                        meta_data['grounding'] = None
+                        meta_data['annot_caption'] = None
+                        loc_pos_list = None
+                    f2.write(json.dumps(meta_data, ensure_ascii=False) + '\n')
+            f1.close()
+            f2.close()
+        print("done")
