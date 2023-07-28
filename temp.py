85068e94ef137e2a5fb98c37b86c7841ec354cf3
Junhui Ji <junhui.ji@aminer.cn>
2023-07-28 15:57:06 +0800

    debug

--- temp.py
+++ temp.py
@@ -21,7 +21,9 @@ def process_dict(g_dict, add_map):
     print(g_dict)
     new_dict = {}
     for obj in g_dict:
+        print(obj)
         for pos in obj:
+            print(pos)
             new_pos = pos + add_map[pos]
             new_dict[obj][new_pos] = g_dict[obj][pos]
     return new_dict
