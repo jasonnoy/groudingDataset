5e17c37bf9ddfb9276b01fe66614ae456c053d9f
Junhui Ji <junhui.ji@aminer.cn>
2023-07-31 19:06:01 +0800

    debug

---
+++ spacy_test.py
@@ -0,0 +1,10 @@
+import spacy
+
+nlp = spacy.load("en_core_web_trf")
+
+text = "Apple is looking at buying U.K. startup for $1 billion"
+doc = nlp(text)
+for t in doc.noun_chunks:
+    print(t.text, len(t.text))
+    print(t[0].text)
+    print(t[0].idx)
