c252c31f384a426d94c6988c328c4eb4b238a396
Junhui Ji <junhui.ji@aminer.cn>
2023-08-01 00:15:25 +0800

    debug

--- spacy_test.py
+++ spacy_test.py
@@ -150,7 +150,7 @@ if __name__ == "__main__":
             p = Process(target=analysis_data_file, args=(in_file_path, out_file_path, err_file_path))
             p.start()
             process_list.append(p)
-            if len(process_list) >= 56:
+            if len(process_list) >= 5:
                 for p in process_list:
                     p.join()
                 process_list = []
