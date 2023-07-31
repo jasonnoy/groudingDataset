ab681dbd00f8bb1cd2fa5c5953fa88263da33408
Junhui Ji <junhui.ji@aminer.cn>
2023-07-31 23:31:27 +0800

    debug

--- spacy_test.py
+++ spacy_test.py
@@ -3,10 +3,9 @@ import torch
 import json
 import os
 import re
-import sys
 import spacy
 from tqdm import tqdm
-import warnings
+from multiprocessing import Process
 import math
 
 
@@ -55,27 +54,8 @@ def get_all_entity_map(entity_lists):
     return res
 
 
-if __name__ == "__main__":
-    torch.multiprocessing.set_start_method('spawn', force=True)
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--local_rank', type=int, default=0)
-    parser.add_argument('--world_size', type=int, default=1)
-    parser.add_argument('--rank', type=int, default=0)
-    parser.add_argument('--master_addr', type=str, default='')
-    parser.add_argument('--master_port', type=int, default=7878)
-    args = parser.parse_args()
-
-    os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:128'
-    os.environ['TOKENIZERS_PARALLELISM'] = 'false'
-    torch.cuda.set_device(args.local_rank)
-
-    # spacy.prefer_gpu(args.local_rank)
-    nlp = spacy.load("en_core_web_trf")
-    path = "/nxchinamobile2/shared/jjh/laion115m_grounding/part-00032/3200467.meta.jsonl"
-    out_path = "/nxchinamobile2/shared/jjh/laion115m-debug/part-00032/3200467.meta.jsonl"
-    err_path = "/nxchinamobile2/shared/jjh/laion115m-debug/part-00032/err_3200467.meta.jsonl"
-    with open(path, "r", encoding='utf-8') as f, open(out_path, 'a', encoding='utf-8') as f2, open(err_path, 'a', encoding='utf-8') as f3:
+def analysis_data_file(in_path, out_path, err_path):
+    with open(in_path, "r", encoding='utf-8') as f, open(out_path, 'a', encoding='utf-8') as f2, open(err_path, 'a', encoding='utf-8') as f3:
         count = 0
         captions = []
         datas = []
@@ -108,30 +88,67 @@ if __name__ == "__main__":
                         old_entity = entity_lists[i][j]
                         offset = entity_offset_cont[i][j]
                         if old_entity in datas[i]['groundings']:
-                            # if all_idx+offset >= len(all_entities):
-                            #     normal = False
-                            #     all_idx += 1
-                            #     continue
+                            if all_idx+offset >= len(all_entities):
+                                normal = False
+                                all_idx += 1
+                                continue
                             new_entity = all_entities[all_idx - offset]
                             groundings[new_entity] = datas[i]['groundings'][old_entity]
                             original_groundings[new_entity] = datas[i]['original_groundings'][old_entity]
                         all_idx += 1
-                    if idx == 19:
-                        # print("entity_lists: ", entity_lists)
-                        # print("entity_offsets: ", entity_offsets)
-                        # print("all_entities: ", all_entities)
-                        print("caption:", captions[i])
-                        print("groundings:", groundings)
+                    # if idx == 19:
+                    #     # print("entity_lists: ", entity_lists)
+                    #     # print("entity_offsets: ", entity_offsets)
+                    #     # print("all_entities: ", all_entities)
+                    #     print("caption:", captions[i])
+                    #     print("groundings:", groundings)
                     if normal:
                         datas[i]['groundings'] = groundings
                         datas[i]['original_groundings'] = original_groundings
                         f2.write(json.dumps(datas[i]) + '\n')
                     else:
                         f3.write(json.dumps(datas[i]) + '\n')
-                if idx == 19:
-                    print("entity_offset_cont:", entity_offset_cont)
+                # if idx == 19:
+                #     print("entity_offset_cont:", entity_offset_cont)
                 count = 0
                 captions = []
                 datas = []
         f2.close()
         f.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--local_rank', type=int, default=0)
+    parser.add_argument('--world_size', type=int, default=1)
+    parser.add_argument('--rank', type=int, default=0)
+    parser.add_argument('--master_addr', type=str, default='')
+    parser.add_argument('--master_port', type=int, default=7878)
+    args = parser.parse_args()
+
+    os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:128'
+    os.environ['TOKENIZERS_PARALLELISM'] = 'false'
+    torch.cuda.set_device(args.local_rank)
+
+    # spacy.prefer_gpu(args.local_rank)
+    nlp = spacy.load("en_core_web_trf")
+    input_path = "/nxchinamobile2/shared/jjh/laion115m_grounding"
+    output_path = "/nxchinamobile2/shared/jjh/laion115m-debug/"
+    process_list = []
+    for dir_i, dir in enumerate(os.listdir(output_path)):
+        print("processing dir {} {}/{}...".format(os.listdir(output_path), dir_i, len(os.listdir(output_path))))
+        output_dir_path = os.path.join(output_path, dir)
+        meta_dir_path = os.path.join(input_path, dir)
+        files = os.listdir(meta_dir_path)
+        for filename in tqdm(files):
+            in_file_path = os.path.join(meta_dir_path, filename)
+            out_file_path = os.path.join(output_dir_path, filename)
+            err_file_path = os.path.join(output_dir_path, "error_"+filename)
+            p = Process(target=analysis_data_file, args=(in_file_path, out_file_path, err_file_path))
+            p.start()
+            process_list.append(p)
+            if len(process_list) >= 256:
+                for p in process_list:
+                    p.join()
+                process_list = []
+
