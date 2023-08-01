d7b5b631a77e0d6c9d177476b881c44f002c1f3f
Junhui Ji <junhui.ji@aminer.cn>
2023-08-01 10:55:44 +0800

    debug

--- scripts/build_debug_wds.py
+++ scripts/build_debug_wds.py
@@ -30,7 +30,7 @@ def write_dataset(meta_path, tar_path, out_path):
 
     sink = webdataset.TarWriter(out_path, encoder=False)
     for line in tar_dataset:
-        if line['id'] in ids:
+        if line['id'].decode() in ids:
             print("write:", {"id": line['id'], "txt": line['txt'], "jpg": line['jpg']})
             sink.write({"id": line['id'], "txt": line['txt'], "jpg": line['jpg']})
     sink.close()
