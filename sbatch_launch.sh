7547c4db4503963d84b50269889a24838f6e80b1
Junhui Ji <junhui.ji@aminer.cn>
2023-08-01 18:50:11 +0800

    debug

--- sbatch_launch.sh
+++ sbatch_launch.sh
@@ -3,9 +3,9 @@
 #SBATCH --output=./logs/build_bounding_dataset_laion_115m_%j.out
 #SBATCH --error=./logs/build_bounding_dataset_laion_115m_%j.err
 #SBATCH --nodes=30
-#SBATCH --gres=gpu:8
-#SBATCH --ntasks-per-node=8
-#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:0
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=40
 #SBATCH --partition=dev
 #SBATCH --export=ALL
 
