5e6150085c40a707ca5859c3d6e7c7b57ed76f87
Junhui Ji <junhui.ji@aminer.cn>
2023-08-01 18:33:09 +0800

    debug

--- scripts/build_from_laion_115m_debug.py
+++ scripts/build_from_laion_115m_debug.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
     id_list.sort()
     finish_ids = []
     for dir in os.listdir(output_path):
-        finish_ids.extend([file.split(sep='.')[0] for file in os.listdir(os.path.join(output_path, dir)) if file.startswith("corrected_") and os.path.getsize(os.path.join(output_path, dir, file)) > 0])
+        finish_ids.extend([file[10:17] for file in os.listdir(os.path.join(output_path, dir)) if file.startswith("corrected_") and os.path.getsize(os.path.join(output_path, dir, file)) > 0])
     # print("finished:", len(finish_ids))
     id_list = list(set(id_list).difference(set(finish_ids)))
 
