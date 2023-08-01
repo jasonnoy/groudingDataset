f4cc2b58ec58f4ed2cbbb46c8516e5030ed865b1
Junhui Ji <junhui.ji@aminer.cn>
2023-08-01 20:17:29 +0800

    debug

--- sbatch_launch.sh
+++ sbatch_launch.sh
@@ -3,13 +3,13 @@
 #SBATCH --output=./logs/build_bounding_dataset_laion_115m_%j.out
 #SBATCH --error=./logs/build_bounding_dataset_laion_115m_%j.err
 #SBATCH --nodes=30
-#SBATCH --gres=gpu:0
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=40
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=5
 #SBATCH --partition=dev
 #SBATCH --export=ALL
 
-srun experiments/correct_data.sh
-#srun experiments/build_from_laion_115m_ningxia.sh
+#srun experiments/correct_data.sh
+srun experiments/build_from_laion_115m_ningxia.sh
 #srun experiments/build_from_laion_aes_jinan.sh
 echo "Done with job $SLURM_JOB_ID"
